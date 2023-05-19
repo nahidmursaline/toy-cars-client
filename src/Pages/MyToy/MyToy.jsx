92c4f49aa3d98c88ac3f53216ed5ead2739a2cb4
nahidmursaline <nahid15-12408@diu.edu.bd>
2023-05-20 03:58:14 +0600

    delete

--- src/Pages/MyToy/MyToy.jsx
+++ src/Pages/MyToy/MyToy.jsx
@@ -19,6 +19,28 @@ const MyToy = () => {
             setToys(data)
         })
     },[])
+
+
+    const handleDelete = id => {
+        const proceed = confirm('Seriously! You Want to DELETE?');
+        if(proceed){
+            fetch(`http://localhost:5000/addToy/${id}`,{
+                method: 'DELETE'
+            })
+            .then(res=> res.json())
+            .then(data => {
+                console.log(data)
+                if(data.deletedCount > 0){
+                    alert('Deleted Successful')
+                    const remaining = toys.filter(toy =>toy._id !== id)
+                    setToys(remaining);
+                }
+            })
+        }
+    }
+    
+
+
     return (
         <div>
             <h2>My Toys</h2>
@@ -47,6 +69,7 @@ const MyToy = () => {
             toys.map(toy=> <MyToyRow
             key={toy._id}
             toy = {toy}
+            handleDelete = {handleDelete}
             ></MyToyRow>)
         }
       
--- src/Pages/MyToy/MyToyRow.jsx
+++ src/Pages/MyToy/MyToyRow.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const MyToyRow = ({toy}) => {
-    const {photo,name, sellerEmail,category,price, rating,quantity,description,sellerName } = toy
+const MyToyRow = ({toy, handleDelete}) => {
+    const {_id,photo,name, sellerEmail,category,price, rating,quantity,description,sellerName } = toy;
+
+
+
+
+
+
+
+
     return (
         <tr>
         <th>
-        <button className="btn btn-sm btn-circle">
+        <button onClick={()=>handleDelete(_id)} className="btn btn-sm btn-circle">
   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
 </button>
         </th>
